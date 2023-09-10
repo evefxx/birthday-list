f29c496dc5f2562803bdcd4798e8b2741c0a707e
evefxx <eve_jive@hotmail.com>
2023-09-10 23:03:16 +0700

    first

---
+++ src/components/List.jsx
@@ -0,0 +1,23 @@
+import React  from "react";
+
+const List = (props) =>{
+    const {people} = props;
+    return (
+        <div>
+            { people.map ((person) => {
+                const { id, name, age, image } = person;
+                return(
+                  <article className="person" key={id}>
+                    <img src={image} alt="" />
+                    <div>
+                       <h4>{name}</h4>
+                       <p>{age}</p>
+                    </div>
+                  </article>
+                );
+              })}
+        </div>
+    );
+}
+
+export default List
---
+++ src/data.js
@@ -0,0 +1,34 @@
+const data = [
+    {
+        id: 1,
+        name: "Bertie Yates",
+        age: 29,
+        image: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80",
+    },
+    {
+        id: 2,
+        name: "Hester Hogan",
+        age: 32,
+        image:  "https://plus.unsplash.com/premium_photo-1668989224643-6b79eaea2108?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
+    },
+    {
+        id: 3,
+        name: "Larry Little",
+        age: 36,
+        image: "https://plus.unsplash.com/premium_photo-1664453546896-ad5e0f895eab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+        id: 4,
+        name: "Sean Walsh",
+        age: 34,
+        image: "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    },
+    {
+        id: 5,
+        name: "Lola Gardner",
+        age: 29,
+        image: "https://images.unsplash.com/photo-1534528741775-53994a69daeb?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+    }
+];
+
+export default data
